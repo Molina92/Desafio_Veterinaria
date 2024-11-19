e3ed4f7eb043ce3b92dc4121ab58871d929102f6
Daniel Molina <danielmolina9202@gmail.com>
2024-11-19 17:10:42 -0300

    modificación del nombre de la variable de los argumentos de entrada

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { registrarCita, leerCitas } = require('./operaciones');
-const argumentos = process.argv.slice(2);
+const datos = process.argv.slice(2);
 
-if (argumentos[0].toLocaleLowerCase() === 'registrar') {
-    registrarCita(argumentos[1], argumentos[2], argumentos[3], argumentos[4], argumentos[5]);
-} else if (argumentos[0].toLocaleLowerCase() === 'leer') {
+if (datos[0].toLocaleLowerCase() === 'registrar') {
+    registrarCita(datos[1], datos[2], datos[3], datos[4], datos[5]);
+} else if (datos[0].toLocaleLowerCase() === 'leer') {
     console.log(leerCitas());
 } else {
     console.log('Comando no reconocido, por favor ingrese "registrar" y los datos de la mascota o "leer" para ver las citas');
